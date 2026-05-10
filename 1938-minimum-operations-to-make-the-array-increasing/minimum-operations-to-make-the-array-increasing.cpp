38736eb500a77d4ee85b5e1c922ec2e6d32a046b
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-05-10 12:26:38 +0530

    Time: 7 ms (83.50%) | Memory: 19.6 MB (62.53%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.cpp
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.cpp
@@ -5,7 +5,7 @@ public:
         for(int i = 1 ; i<nums.size() ; i++){
             if(nums[i]<=nums[i-1]){
                 ans += (nums[i-1]-nums[i]+1) ;
-                nums[i] += nums[i-1]-nums[i]+1 ;
+                nums[i]=max(nums[i-1]+1, nums[i]);
             }
         }
         return ans ;
