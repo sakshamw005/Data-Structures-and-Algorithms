e1a76a492bcdcacc3bd2bf8a33a5164b52eed7da
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-05-28 22:00:53 +0530

    Time: 68 ms (38.12%) | Memory: 282.2 MB (85.08%) - LeetSync

---
+++ 3376-longest-common-suffix-queries/longest-common-suffix-queries.java
@@ -0,0 +1,64 @@
+class Solution {
+    int[] lens;
+    public int[] stringIndices(String[] wordsContainer, String[] wordsQuery) {
+        lens = new int[wordsContainer.length];
+        Trie trie = new Trie();
+
+        for (int i = 0; i < wordsContainer.length; i++) {
+            lens[i] = wordsContainer[i].length();
+            trie.insert(reverse(wordsContainer[i]), i);
+        }
+
+        int[] ans = new int[wordsQuery.length];
+        for (int i = 0; i < wordsQuery.length; i++) {
+            ans[i] = trie.search(reverse(wordsQuery[i]));
+        }
+        return ans;
+    }
+
+    private String reverse(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+
+    class TrieNode {
+        TrieNode[] child = new TrieNode[26];
+        int bestIndex = -1;
+    }
+
+    class Trie {
+        TrieNode root = new TrieNode();
+
+        void insert(String word, int index) {
+            TrieNode curr = root;
+            update(curr, index);
+
+            for (char ch : word.toCharArray()) {
+                int c = ch - 'a';
+                if (curr.child[c] == null) curr.child[c] = new TrieNode();
+                curr = curr.child[c];
+                update(curr, index);
+            }
+        }
+
+        int search(String word) {
+            TrieNode curr = root;
+            int ans = curr.bestIndex;
+
+            for (char ch : word.toCharArray()) {
+                int c = ch - 'a';
+                if (curr.child[c] == null) break;
+                curr = curr.child[c];
+                ans = curr.bestIndex;
+            }
+            return ans;
+        }
+
+        void update(TrieNode node, int index) {
+            if (node.bestIndex == -1 ||
+                lens[index] < lens[node.bestIndex] ||
+                (lens[index] == lens[node.bestIndex] && index < node.bestIndex)) {
+                node.bestIndex = index;
+            }
+        }
+    }
+}
