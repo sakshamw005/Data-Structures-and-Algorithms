67dee0feec07ef9c20069665639a91b0c0fce67f
Saksham Wadhwa <143003056+sakshamw005@users.noreply.github.com>
2026-07-12 12:34:26 +0530

    Time: 76 ms (14.16%) | Memory: 44.5 MB (43.71%) - LeetSync

---
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> a = arr ;
+        if(arr.size()==0)return {} ;
+        sort(a.begin(),a.end()) ;
+        map<int,int> um; 
+        um[a[0]] = 1 ;
+        for(int i = 1; i<a.size() ; i++){
+            if(a[i] > a[i-1]){
+                um[a[i]] = um[a[i-1]] + 1 ;
+            }
+        } 
+        vector<int> ans(arr.size()) ;
+        for(int i =0 ;  i<arr.size() ; i++){
+            ans[i] = um[arr[i]] ;
+        }
+        return ans ;
+    }
+};
